bd79042a141e716c7c4383a7beea3154d434ba65
hyesech <hyesech@gmail.com>
2022-05-08 20:30:40 +0900

    chore: 핫로더, TS Checker 세팅 및 로컬 서버 명령어 설정
    - Webpack Dev Server Configuration 인식 문제 해결 위해 Configuration interface 재선언 후 devServer 추가

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,19 @@
 import path from 'path';
-import webpack from 'webpack';
+import webpack, { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+/* Webpack Dev Server 인식 문제로 따로 Webpack Configuration interface 다시 선언
+ *   -> Webpack Dev Server Configuration 추가
+ * */
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-const config: webpack.Configuration = {
+const config: Configuration = {
   name: 'socket-hang-out',
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'hidden-source-map' : 'eval',
@@ -37,25 +47,47 @@ const config: webpack.Configuration = {
             '@babel/preset-react',
             '@babel/preset-typescript',
           ],
-          plugins: [
-            // TODO: 추후 핫로더 같은 거 추가할 예정
-          ],
+          env: {
+            // development 플러그인 자리
+            development: {
+              plugins: [[require.resolve('react-refresh/babel')]],
+            },
+            production: {
+              // production 플러그인 자리
+              plugins: [],
+            },
+          },
         },
         exclude: path.join(__dirname, 'node_modules'),
       },
     ],
   },
-  // TODO: TsChecker 플러그인 추가할 예정
+  // TsChecker 플러그인 - ts랑 webpack 병렬 처리되도록 해주는 플러그인 원래 ts checker == blocking
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+    new webpack.EnvironmentPlugin({ NODE_ENV: isDevelopment ? 'development' : 'production' }),
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
-    publicPath: '/dist',
+    publicPath: '/dist/',
   },
-  //  TODO: Dev Server 설정할 예정
+  // Dev Server 설정
+  devServer: {
+    historyApiFallback: true, // React router
+    port: 8080,
+    devMiddleware: { publicPath: '/dist/' },
+    static: { directory: path.resolve(__dirname) },
+    //  이후 필요한 경우 proxy 세팅도 여기에 할 거임
+  },
 };
 
 // plugin push 예정
 if (isDevelopment && config.plugins) {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.plugins.push(new ReactRefreshPlugin());
 }
 
 export default config;
